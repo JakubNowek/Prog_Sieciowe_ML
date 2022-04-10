cd600c743074918f8aa5ea57568da181137ac1fc
JakubNowek <jakubbnwek@gmail.com>
2022-04-10 23:36:33 +0200

    gotowe do robienia sprawka i testow

--- Programy/zad3_3klasy_perceptron.py
+++ Programy/zad3_3klasy_perceptron.py
@@ -78,9 +78,9 @@ X_test_std = np.copy(X_test)
 X_test_std[:, 0] = (X_test[:, 0] - X_test[:, 0].mean()) / X_test[:, 0].std()
 X_test_std[:, 1] = (X_test[:, 1] - X_test[:, 1].mean()) / X_test[:, 1].std()
 
-ppn_setosa = Perceptron(epochs=100, eta=0.1)  # tworzenie nowego perceptronu rozpoznającego setosy
-ppn_versicolor = Perceptron(epochs=300, eta=0.000001)  # tworzenie nowego perceptronu rozpoznającego versicolory
-ppn_virginica = Perceptron(epochs=100, eta=0.1)  # tworzenie nowego perceptronu rozpoznającego virginice
+ppn_setosa = Perceptron(epochs=10, eta=0.01)  # tworzenie nowego perceptronu rozpoznającego setosy
+ppn_versicolor = Perceptron(epochs=10, eta=0.001)  # tworzenie nowego perceptronu rozpoznającego versicolory
+ppn_virginica = Perceptron(epochs=100, eta=0.01)  # tworzenie nowego perceptronu rozpoznającego virginice
 
 ppn_setosa.train(X_train_std, y_tr_setosa)
 ppn_versicolor.train(X_train_std, y_tr_versicolor)
@@ -90,18 +90,49 @@ ppn_virginica.train(X_train_std, y_tr_virginica)
 # print('Weights: %s' % ppn_versicolor.w_)
 # print('Weights: %s' % ppn_virginica.w_)
 
-# plot_decision_regions(X_training, y_training, clf=ppn)
+plot_decision_regions(X_train_std, y_tr_setosa, clf=ppn_setosa)
 # plt.title('Perceptron')
 # plt.xlabel('sepal length [cm]')
 # plt.ylabel('petal length [cm]')
-# plt.show()
-#
+
+plot_decision_regions(X_train_std, y_tr_versicolor, clf=ppn_versicolor)
+# plt.title('Perceptron')
+# plt.xlabel('sepal length [cm]')
+# plt.ylabel('petal length [cm]')
+
+plot_decision_regions(X_train_std, y_tr_virginica, clf=ppn_virginica)
+plt.title('Perceptron')
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.show()
 # plt.plot(range(1, len(ppn.errors_)+1), ppn.errors_, marker='o')
 # plt.xlabel('Iterations')
 # plt.ylabel('Misclassifications')
 # plt.show()
 
 # testowanie nauczonego perceptronu
-print(ppn_setosa.predict(X_test_std))
-print(ppn_versicolor.predict(X_test_std))
-print(ppn_virginica.predict(X_test_std))
+result_set = list(ppn_setosa.predict(X_test_std))
+print(result_set)
+result_ver = list(ppn_versicolor.predict(X_test_std))
+print(result_ver)
+result_vir = list(ppn_virginica.predict(X_test_std))
+print(result_vir)
+
+# ostateczny wynik algorytmu? jeśli wykryto w jednej to nazwa, a jeśli w dwóch to brak nazwy
+result_array = np.array([result_set, result_ver, result_vir])
+result_list = []
+print(result_array)
+array_sum = np.sum(result_array, axis=0)
+# result = list(np.zeros(len(X_test_std)))
+for i in range(np.shape(result_array)[1]):  # iterowanie po ilości kolumn
+    if array_sum[i] == 1:  # sumowanie elementów w kolumnach
+        if result_set[i] == -1:
+            result_list.append('Set')
+        if result_ver[i] == -1:
+            result_list.append('Ver')
+        if result_vir[i] == -1:
+            result_list.append('Vir')
+    else:
+        result_list.append('-')
+print(result_list)
+
