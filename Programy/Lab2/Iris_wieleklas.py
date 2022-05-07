dcda0e1493d8a04d285b9c88680aab9bde6be87a
JakubNowek <jakubbnwek@gmail.com>
2022-05-07 16:08:10 +0200

    first task finished i guess

--- Programy/Lab2/Iris_wieleklas.py
+++ Programy/Lab2/Iris_wieleklas.py
@@ -15,16 +15,6 @@ warnings.filterwarnings('ignore')
 # pobieranie danych w formacie csv
 iris = datasets.load_iris()
 
-mlp = MLPClassifier(activation='tanh', learning_rate='constant')
-parameters = {
-    'hidden_layer_sizes': [20,40,60,80,100],
-    'solver': ['adam'],
-    'learning_rate_init': [0.1, 0.01, 0.001]
-}
-mlp.out_activation_ = 'softmax'
-clf = GridSearchCV(mlp, parameters)
-
-
 # zbiór treningowy
 iris_tr = np.concatenate([iris.data[:40], iris.data[50:90], iris.data[100:140]])
 iris_tr_target = np.concatenate([iris.target[:40], iris.target[50:90], iris.target[100:140]])
@@ -33,14 +23,32 @@ iris_tr_target = np.concatenate([iris.target[:40], iris.target[50:90], iris.targ
 iris_test = np.concatenate([iris.data[40:50], iris.data[90:100], iris.data[140:150]])
 iris_test_real = np.concatenate([iris.target[40:50], iris.target[90:100], iris.target[140:150]])
 
+# tworzenie wielowarstwowego perceptronu
+mlp = MLPClassifier(activation='tanh', learning_rate='constant')
+parameters = {
+    'hidden_layer_sizes': [20,40,60,80,100],
+    'solver': ['adam'],
+    'learning_rate_init': [0.01, 0.001]
+}
+mlp.out_activation_ = 'softmax'
+clf = GridSearchCV(mlp, parameters)
+
+
+
+
 # trenowanie
 clf.fit(iris_tr, iris_tr_target)
-print(clf.get_params())
-#print(clf.cv_results_)
+# wypisywanie najlepszych znalezionych parametrów
+print("Parametry: \n",clf.get_params())
+# ustawienie widoczności całego DataFrame'a
+cvResultsDF = pd.DataFrame(clf.cv_results_)
+pd.set_option("display.max_rows", None,"display.max_columns", None,
+              "max_colwidth", None, "display.expand_frame_repr", False)
+# cvresult wypisuje wszystkie możliwości, więc będzie tyle wierszy ile możliwości, czyli tutaj
+print("Grid search results: \n", cvResultsDF)
 
 # przewidywanie
 iris_pred = clf.predict(iris_test)
-
 #macierz pomyłek
 cm = confusion_matrix(iris_pred, iris_test_real)
 print("Prediction output: \n", iris_pred)
