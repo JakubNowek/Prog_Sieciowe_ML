b6cfb4a50bc29565080c9e59f496ec44e4089e6f
JakubNowek <jakubbnwek@gmail.com>
2022-06-18 20:41:21 +0200

    display partialy working

--- Programy/Lab4/Kohonen.py
+++ Programy/Lab4/Kohonen.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+#from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 
 
-def kohonen(p, alpha_0, dane, ile_razy_T=5):
+def kohonen(p, alpha_0, dane, ile_razy_T=10):
 
     N = len(dane)
     T = ile_razy_T
@@ -60,22 +60,41 @@ def kohonen(p, alpha_0, dane, ile_razy_T=5):
 iris = pd.read_csv('iris.data', header=None)
 iris = iris.iloc[:, [0, 1, 2]]
 
-wektory = kohonen(p=3, alpha_0=0.1, dane=iris)
+wektory = kohonen(p=3, alpha_0=0.6, dane=iris)
 
 for i in range(len(wektory)):
     wektory[i] = wektory[i].tolist()
 
 
 fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.set_xlim = ([0, 2])
-ax.set_ylim = ([0, 2])
-ax.set_zlim = ([0, 2])
+ax = fig.add_subplot(111, projection='3d')
+
 
 start = [0, 0, 0]
 
-ax.quiver(start[0], start[1], start[2], wektory[0][0], wektory[0][1], wektory[0][2])
-# ax.quiver(start[0], start[1], start[2], wektory[1][0], wektory[1][1], wektory[1][2])
-# ax.quiver(start[0], start[1], start[2], wektory[2][0], wektory[2][1], wektory[2][2])
+limity = plt.gca()
+limity.set_xlim([-1, 2])
+limity.set_ylim([-1, 2])
+limity.set_zlim([-1, 2])
 
+
+suma = 0
+for i in range(len(iris)):
+    suma += iris.iloc[i]
+offset = suma / len(iris)
+# odejmowanie offsetu i normalizacja (dzielenie każdego wektora przez jego normę euklidesową)
+for i in range(len(iris)):
+    iris.iloc[i] = (offset - iris.iloc[i]) / np.linalg.norm(iris.iloc[i])
+for i in range(len(iris)):
+    ax.scatter(iris.iloc[i][0], iris.iloc[i][1], iris.iloc[i][2])
+
+ax.quiver(start[0], start[1], start[2], wektory[0][0], wektory[0][1], wektory[0][2], )
+ax.quiver(start[0], start[1], start[2], wektory[1][0], wektory[1][1], wektory[1][2])
+ax.quiver(start[0], start[1], start[2], wektory[2][0], wektory[2][1], wektory[2][2])
+
+
+for angle in range(0, 360):
+    ax.view_init(30, angle)
+    plt.draw()
+    plt.pause(.001)
 
