24df60878f8c103f7acf62b12002ec5036e150c1
JakubNowek <jakubbnwek@gmail.com>
2022-06-20 23:24:28 +0200

    added collecting data from 5 last days as input

--- Programy/Lab4/Kohonen_stocks.py
+++ Programy/Lab4/Kohonen_stocks.py
@@ -8,7 +8,6 @@ from sklearn.metrics import davies_bouldin_score
 from sklearn.manifold import TSNE  # do zmiany wymiarów
 from scipy.spatial.distance import cityblock  # do liczenia normy Manhattan
 
-
 def norm1(wr_list, dane, ile_klas, index):
     temp = []
     for m in range(ile_klas):
@@ -54,14 +53,13 @@ def kohonen(p, alpha_0, dane, ile_razy_T=10):
     for i in range(len(dane)):
         suma += dane.iloc[i]
     offset = suma / N
-    # odejmowanie offsetu i normalizacja (dzielenie każdego wektora przez jego normę euklidesową)
+    # odejmowanie offsetu
     for i in range(len(dane)):
-        dane.iloc[i] = (offset-dane.iloc[i])/np.linalg.norm(dane.iloc[i])
+        dane.iloc[i] = (offset-dane.iloc[i])
 
-    print(dane)
     # inicjalizacja wektorów reprezentantów
     for j in range(p):
-        wr_list.append(1/np.sqrt(N)*np.ones(howManyCols))
+        wr_list.append(1/math.sqrt(N)*np.ones(howManyCols))
     #print('Wektory repr przed uczeniem:', wr_list,'\n')
 
     alpha_k = alpha_0
@@ -70,8 +68,8 @@ def kohonen(p, alpha_0, dane, ile_razy_T=10):
     for k in range(T):
         # wyznaczanie miary i przypisywanie punktom numeru wektora
         for i in range(len(dane)):
-            # temp = norm1(wr_list, dane, p, i)
-            temp = norm2(wr_list, dane, p, i)
+            temp = norm1(wr_list, dane, p, i)
+            #temp = norm2(wr_list, dane, p, i)
             #temp = norm_manh(wr_list, dane, p, i)
             m_list.append(temp)
 
@@ -84,11 +82,11 @@ def kohonen(p, alpha_0, dane, ile_razy_T=10):
             wr_list[m_list[i]] = wr_list[m_list[i]] / np.linalg.norm(wr_list[m_list[i]])
 
         # zmniejszanie liniowe alpha
-        #alpha_k = alpha_0*(T-k)/T
+        alpha_k = alpha_0*(T-k)/T
         # zmniejszanie wykładnicze alpha
         #alpha_k = alpha_0*math.exp(-C*k)
         # zmniejszanie hiperboliczne alpha
-        alpha_k = C1/(C2 + k)
+        #alpha_k = C1/(C2 + k)
 
     #print('Wektory repr po uczeniu:\n', wr_list)
     return wr_list
@@ -98,70 +96,84 @@ def kohonen(p, alpha_0, dane, ile_razy_T=10):
 stonks = pd.read_csv('KIM_data.csv', usecols=['open', 'high', 'low'])
 # stonks = pd.read_csv('KIM_data.csv', usecols=['open', 'high', 'low', 'close', 'volume'])
 
-# =============================wywołanie======================================= #
-wektory = kohonen(p=3, alpha_0=0.1, dane=stonks)
-# ============================================================================= #
-
-# zamiana macierzy array na listę wektorów
-for i in range(len(wektory)):
-    wektory[i] = wektory[i].tolist()
-
-# NORMALIZACJA danych do plotowania na wykresie z wektorami
-suma = 0
+stonks_cpy = stonks.copy()
+# 5 dni do jednego
+ile_dni = 5
 for i in range(len(stonks)):
-    suma += stonks.iloc[i]
-offset = suma / len(stonks)
-# odejmowanie offsetu i normalizacja (dzielenie każdego wektora przez jego normę euklidesową)
-for i in range(len(stonks)):
-    stonks.iloc[i] = (offset - stonks.iloc[i]) / np.linalg.norm(stonks.iloc[i])
+    if i - ile_dni >= 0:
+        for j in range(1, ile_dni):
+            stonks.iloc[i] += stonks_cpy.iloc[i-j]
+        stonks.iloc[i] = stonks.iloc[i]/ile_dni
+    else:
+        stonks.iloc[i] = "NaN"
 
-
-# PLOTTOWANIE dla 3d
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-start = [0, 0, 0]
-
-limity = plt.gca()
-limity.set_xlim([-1, 2])
-limity.set_ylim([-1, 2])
-limity.set_zlim([-1, 2])
-
-
-# iris_set = iris.iloc[:50]
-# iris_ver = iris.iloc[50:100]
-# iris_vir = iris.iloc[100:150]
-
-
-for i in range(len(stonks)):
-    ax.scatter(stonks.iloc[i][0], stonks.iloc[i][1], stonks.iloc[i][2])
-
-
-
-
-ax.quiver(start[0], start[1], start[2], wektory[0][0], wektory[0][1], wektory[0][2], )
-ax.quiver(start[0], start[1], start[2], wektory[1][0], wektory[1][1], wektory[1][2])
-# ax.quiver(start[0], start[1], start[2], wektory[2][0], wektory[2][1], wektory[2][2])
-ax.view_init(0,45)
-plt.show()
-
-
-# DAVIES-BOULDIN
-# szacowanie rzeczywistej liczby klas (minimum funkcji to najbardziej prawdopodobna liczba klas)
-results = {}
-search_range = [2, 15]
-for i in range(search_range[0], search_range[1]):  # sprawdzamy minimum dla liczby klas do 2 do 15
-    kmeans = KMeans(n_clusters=i, random_state=30)
-    labels = kmeans.fit_predict(stonks)
-    db_index = davies_bouldin_score(stonks, labels)
-    results.update({i: db_index})
-
-plt.plot(list(results.keys()), list(results.values()))
-plt.xlabel("Number of clusters")
-plt.ylabel("Davies-Bouldin Index")
-plt.show()
-
-# wyznaczanie liczby klas, przy których wskaźnik jest najmniejszy
-wskazniki = list(results.values())
-print(wskazniki)
-liczba_klas = wskazniki.index(min(wskazniki)) + search_range[0]
-print('Oszacowana liczba klas: ', liczba_klas)
+# wyrzucanie nieprzydatnych danych
+stonks = stonks.iloc[5:]
+stonks = stonks.reset_index(drop=True)
+print(stonks)
+# # =============================wywołanie======================================= #
+# wektory = kohonen(p=3, alpha_0=0.6, dane=stonks)
+# # ============================================================================= #
+#
+# # zamiana macierzy array na listę wektorów
+# for i in range(len(wektory)):
+#     wektory[i] = wektory[i].tolist()
+#
+# # NORMALIZACJA danych do plotowania na wykresie z wektorami
+# suma = 0
+# for i in range(len(stonks)):
+#     suma += stonks.iloc[i]
+# offset = suma / len(stonks)
+# # odejmowanie offsetu i normalizacja (dzielenie każdego wektora przez jego normę euklidesową)
+# for i in range(len(stonks)):
+#     stonks.iloc[i] = (offset - stonks.iloc[i])
+#
+#
+# # PLOTTOWANIE dla 3d
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# start = [0, 0, 0]
+#
+# limity = plt.gca()
+# limity.set_xlim([-10, 10])
+# limity.set_ylim([-10, 10])
+# limity.set_zlim([-10, 15])
+#
+#
+# # iris_set = iris.iloc[:50]
+# # iris_ver = iris.iloc[50:100]
+# # iris_vir = iris.iloc[100:150]
+#
+#
+# for i in range(len(stonks)):
+#     ax.scatter(stonks.iloc[i][0], stonks.iloc[i][1], stonks.iloc[i][2])
+#
+#
+# ax.quiver(start[0], start[1], start[2], wektory[0][0], wektory[0][1], wektory[0][2], )
+# ax.quiver(start[0], start[1], start[2], wektory[1][0], wektory[1][1], wektory[1][2])
+# # ax.quiver(start[0], start[1], start[2], wektory[2][0], wektory[2][1], wektory[2][2])
+# ax.view_init(0,25)
+#
+# plt.show()
+#
+#
+# # DAVIES-BOULDIN
+# # szacowanie rzeczywistej liczby klas (minimum funkcji to najbardziej prawdopodobna liczba klas)
+# results = {}
+# search_range = [2, 15]
+# for i in range(search_range[0], search_range[1]):  # sprawdzamy minimum dla liczby klas do 2 do 15
+#     kmeans = KMeans(n_clusters=i, random_state=30)
+#     labels = kmeans.fit_predict(stonks)
+#     db_index = davies_bouldin_score(stonks, labels)
+#     results.update({i: db_index})
+#
+# plt.plot(list(results.keys()), list(results.values()))
+# plt.xlabel("Number of clusters")
+# plt.ylabel("Davies-Bouldin Index")
+# plt.show()
+#
+# # wyznaczanie liczby klas, przy których wskaźnik jest najmniejszy
+# wskazniki = list(results.values())
+# print(wskazniki)
+# liczba_klas = wskazniki.index(min(wskazniki)) + search_range[0]
+# print('Oszacowana liczba klas: ', liczba_klas)
