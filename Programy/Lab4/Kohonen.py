c9cd2ef752e1149e25dbabcb566d89ed7dcc94df
JakubNowek <jakubbnwek@gmail.com>
2022-06-20 18:53:02 +0200

    added Davies-Bouldin method for determining number of clusters

--- Programy/Lab4/Kohonen.py
+++ Programy/Lab4/Kohonen.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 #from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
-
+from sklearn.cluster import KMeans
+from sklearn.metrics import davies_bouldin_score
 
 def kohonen(p, alpha_0, dane, ile_razy_T=10):
 
@@ -62,22 +63,11 @@ iris = iris.iloc[:, [0, 1, 2]]
 
 wektory = kohonen(p=3, alpha_0=0.6, dane=iris)
 
+# zamiana macierzy array na listę wektorów
 for i in range(len(wektory)):
     wektory[i] = wektory[i].tolist()
 
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-
-start = [0, 0, 0]
-
-limity = plt.gca()
-limity.set_xlim([-1, 2])
-limity.set_ylim([-1, 2])
-limity.set_zlim([-1, 2])
-
-
+# NORMALIZACJA IRIS
 suma = 0
 for i in range(len(iris)):
     suma += iris.iloc[i]
@@ -85,6 +75,18 @@ offset = suma / len(iris)
 # odejmowanie offsetu i normalizacja (dzielenie każdego wektora przez jego normę euklidesową)
 for i in range(len(iris)):
     iris.iloc[i] = (offset - iris.iloc[i]) / np.linalg.norm(iris.iloc[i])
+
+
+# PLOTTOWANIE
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+start = [0, 0, 0]
+
+limity = plt.gca()
+limity.set_xlim([-1, 2])
+limity.set_ylim([-1, 2])
+limity.set_zlim([-1, 2])
+
 for i in range(len(iris)):
     ax.scatter(iris.iloc[i][0], iris.iloc[i][1], iris.iloc[i][2])
 
@@ -92,9 +94,19 @@ ax.quiver(start[0], start[1], start[2], wektory[0][0], wektory[0][1], wektory[0]
 ax.quiver(start[0], start[1], start[2], wektory[1][0], wektory[1][1], wektory[1][2])
 ax.quiver(start[0], start[1], start[2], wektory[2][0], wektory[2][1], wektory[2][2])
 
+plt.show()
 
-for angle in range(0, 360):
-    ax.view_init(30, angle)
-    plt.draw()
-    plt.pause(.001)
+
+# szacowanie rzeczywistej liczby klas (minimum funkcji to najbardziej prawdopodobna liczba klas)
+results = {}
+for i in range(2,11):
+    kmeans = KMeans(n_clusters=i, random_state=30)
+    labels = kmeans.fit_predict(iris)
+    db_index = davies_bouldin_score(iris, labels)
+    results.update({i: db_index})
+
+plt.plot(list(results.keys()), list(results.values()))
+plt.xlabel("Number of clusters")
+plt.ylabel("Davies-Boulding Index")
+plt.show()
 
