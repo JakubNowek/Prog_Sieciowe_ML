67ff0e880a4595b6de8ec65bcb90dd66f7c2e5a0
JakubNowek <jakubbnwek@gmail.com>
2022-06-28 14:29:18 +0200

    added comments

--- Programy/Lab5/Kohonen_itercontinental.py
+++ Programy/Lab5/Kohonen_itercontinental.py
@@ -6,42 +6,47 @@ from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import davies_bouldin_score, calinski_harabasz_score
-from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from itertools import combinations
 
 
-# Algorytm Kohonena robi klasteryzacje. Jest to metoda dokonująca grupowania elementów we względnie jednorodne klasy.
-# Innymi słowy algorytm bierze kolejne próbki (X), liczbę klastrów i przypisuje każdej próbce pasujący klaster.
-class Kohonenalgorithm:
+class Kohonen:
     # Atrybuty nadawane przy tworzeniu obiektu.
-    def __init__(self, p:int, method="cosine", alpha_method = "linear"):
-        """ p - liczba skupisk danych """
+    def __init__(self, p:int, method="cosine", alpha_method="linear"):
+        """
+        @param p: liczba klas
+        @param method: jedna z 3 metod do wyznaczania normy
+        @param alpha_method: jedna z 3 metod aktualizacji współczynnika uczenia
+        """
         self.n_clusters = p
         self.method = method
         self.alpha_method = alpha_method
 
-    # metoda realizująca algorytm Kohonena.
     def fit_transform(self, X, iterations, alpha_init):
-        # inicjalizacja współczynnika uczenia z metody __init__().
+        """
+        @param X: dane
+        @param iterations: liczba iteracji
+        @param alpha_init: początkowa wartość współczynnika uczenia
+        @return: tablica o rozmiarze N, przechowująca dla każdej próbki wartość klastra, do którego należy
+        """
+        # współczynnik uczenia
         alpha = alpha_init
-         # obliczanie środka ciężko wg prezentacji.
+        # środek ciężkości zbioru
         s = X.sum(axis=0)/len(X)
-        # normalizacja danych wg prezentacji.
-        X_normalized = np.array([(s - x)/np.linalg.norm(x, axis=-1) for x in X])
-        # inicjalizacja wektorów "w- centroidy" wg prezentacji.
+        # normalizacja danych
+        X_norm = np.array([(s - x)/np.linalg.norm(x, axis=-1) for x in X])
+        # inicjowanie wektorów w (wektor w jest centroidą)
         w = np.random.random((self.n_clusters, len(X[0])))
-        w = w /np.linalg.norm(w)
+        w = w / np.linalg.norm(w)
 
         for k in tqdm.tqdm(range(iterations)):
-            # przypisanie numeru klastru do próbki poprzez wyliczenie odległości próbek od skupisk.
-            # Najmniejsza odległość od danego klastru spowoduje przypisanie do niego danej próbki.
+            # wyznaczanie miary i przypisywanie punktom numeru wektora
             if self.method =="cosine":
-                m = np.argmax([[wi.T@x for wi in w] for x in X_normalized], axis=1)
+                m = np.argmax([[wi.T@x for wi in w] for x in X_norm], axis=1)
             elif self.method =="euklidean":
-                m = np.argmin([[np.linalg.norm(wi - x) for wi in w] for x in X_normalized], axis=1)
+                m = np.argmin([[np.linalg.norm(wi - x) for wi in w] for x in X_norm], axis=1)
             elif self.method =="manhatan":
-                m = np.argmin([[sqrt(sum(np.abs(wi - x))) for wi in w] for x in X_normalized], axis=1)
+                m = np.argmin([[sqrt(sum(np.abs(wi - x))) for wi in w] for x in X_norm], axis=1)
             else:
                 raise NotImplementedError
 
@@ -56,116 +61,90 @@ class Kohonenalgorithm:
                 alpha = C_1/(C_2 + k)
             else:
                 raise NotImplementedError
-            # aktualizacja wag wg prezentacji. Wagi (centroidy) są początkowo
-            # inicjalizowane w pewnym punkcie, po czym się aktualizują, czyli zbliżają sie do skupisk.
-            for x, i in zip(X_normalized, m):
-                w[i] = w[i] + alpha*(x - w[i])
+            # # aktualizowanie wektorów reprezentantów
+            for x, i in zip(X_norm, m):
+                w[i] = w[i] + alpha * (x - w[i])
+                # normalizacja wektorów rep
                 w[i] = w[i] / np.linalg.norm(w[i])
-
-            # m jest tablicą. Kolejne elementy tablicy "m" odpowiadają danym X.
-            # w każdym elemencie tablicy m jest numer klastru, do którego została przydzielona dana próbka.
         return m
 
-    # funkcja zwracają tablicę z wyliczonymi środkami ciężkości klastrów.
+    # zwracanie tablicy wektorów - każdy wektor zawiera środek ciężkości klastra
     def get_centroids(self, X, iterations, alpha_init):
-        # wywołanie algorytmu Kohonena. Zdobycie tablicy m, która mówi nam o przynależności próbek do poszczególnych klastrów.
         m = self.fit_transform(X, iterations, alpha_init)
-        # podział danych na klastry.
+        # rozdzielenie danych wg przynależności do klastrów
         data_by_cluster = [np.array([sample for sample, cluster_nr in zip(X, m) if cluster_nr == i]) for i in range(self.n_clusters)]
-        # obliczanie centroid jako środków cięzkości zbiorów. środek ciężkości jest to środek poszczególnych skupisk.
+        # obliczanie środków ciężkości klas
         centroids = [cluster.sum(axis=0)/len(cluster) for cluster in data_by_cluster if cluster.any()]
         return centroids
 
 
-# Liczenie funkcji radialnej Phi.
+# funkcja aktywacji - zwraca funkcję, z parametrem x, i stałą r
 def get_phi(r: float):
     def phi(x: float) -> float:
         return exp(-(x / r) ** 2)
-
     return phi
 
 
-# Liczenie objętości zbioru (średnicy - r).
+# funkcja wyznaczająca średnicę całego zbioru
 def diameter(Points):
     diam = max([sqrt(np.sum((p - q) ** 2, axis=None)) for p, q in tqdm.tqdm(combinations(Points, 2))])
     return diam
 
 
-# Aproksymacja na podstawie parametrów parametrów w,c,r zdobytych w funkcji RBF_approximation.
-# Aproksymacja jest liczona na podstawie powyższego wzoru.
-def get_approximation(w, c, r):
-    # konstrukcja funkcji phi (funkcja radialna).
+# aproksymacja na podstawie w, c, r
+def get_approx(w, c, r):
+    """
+    funkcja aproksymująca
+    @param w: wektor wag sieci
+    @param c: wektor centrów (ma p elementów)
+    @param r: średnica całego zbioru
+    @return: wyjście sieci y(x)
+    """
     phi = get_phi(r)
     return lambda x: np.sum([w[i] * phi(np.linalg.norm(x - c[i])) for i in range(len(c))])
 
 
-""" Algorytm algebraiczny liczenia wag. """
-# Funkcja zwraca w - wektor wag, c - centroidy, r - średnica zbioru.
-def RBF_approximation(X, y, p=20):
-    """ Funkcja oblicza wagi aproksymacji RBF na podstawie danych X, etykiet y i liczby klastów p"""
+def RBF_iter(X, y, p=20, n_iterations=200, l_r=0.1):
+    """
+    Algorytm iteracyjny liczenia wag
+    @param X: dane wejściowe
+    @param y: pożądane wyniki
+    @param p: liczba klastrów
+    @param n_iterations: ilość powtórzeń
+    @param l_r: prędkość uczenia
+    @return: wektor wag, wektor centrów, średnica zbioru
+    """
     # definicja tablicy F
     F = y
 
-    # klasteryzacja zbioru i obliczenie centroid.
+    # klasteryzacja zbioru i obliczenie centroid
     print("Data clustering")
-    c = Kohonenalgorithm(p=p).get_centroids(X, iterations=100, alpha_init=0.5)
-
-    # ustalenie parametru r = średnica zbioru.
+    c = Kohonen(p).get_centroids(X, iterations=100, alpha_init=0.2)
+    print("Calculating the set diameter")
     r = diameter(X)
 
-    # konstrukcja funkcji phi (funkcja radialna).
+    # konstrukcja funkcji phi
     phi = get_phi(r)
 
-    # definicja tablicy Phi wielkiego (ze wzoru w=Phi*F)
-    # liczba wierszy = liczba danych x  (1...N)
-    # liczba kolumn = liczba centroid   (1...p)
-    Phi = np.array([[phi(np.linalg.norm(X[j] - c[i])) for i in range(len(c))] for j in range(len(X))])
-    # Obliczenie wektora w na podstawie wzoru w=Phi^(-1)*F. Phi^(-1) - pseudoodwrotność
-    w = np.linalg.pinv(Phi) @ F
+    # początkowe losowe inicjowanie wag
+    w = np.random.rand(len(c))
+
+    # aktualizowanie wag - część iteracyjna
+    print("Learning")
+    for i in tqdm.tqdm(range(n_iterations)):
+      approx_fcn = get_approx(w, c, r)
+      for sample, label in zip(X, y):
+          y_pred = approx_fcn(sample)
+          for j in range(len(w)):
+              w[j] = w[j] + l_r * (label - y_pred) * phi(np.linalg.norm(sample - c[j]))
     return w, c, r
 
 
-""" Algorytm iteracyjny liczenia wag. """
-
-def RBF_approximation_iter(X, y, p=20, n_iterations=200, l_r=0.1):
-  """ Funkcja oblicza wagi aproksymacji RBF na podstawie danych X, etykiet y i liczby klastów p"""
-  # definicja tablicy F
-  F = y
-
-  # klasteryzacja zbioru i obliczenie centroid
-  print("Data clustering")
-  c = Kohonenalgorithm(p=p).get_centroids(X, iterations=100, alpha_init=0.2)
-  # c = KMeans(n_clusters=p, random_state=0).fit(X).cluster_centers_
-
-  # r = średnica zbioru
-  # ustalenie parametru r
-  print("Calculating the set diameter")
-  r = diameter(X)
-
-  # konstrukcja funkcji phi
-  phi = get_phi(r)
-
-  # losowa inicjalizacja wag.
-  w = np.random.rand(len(c))
-
-  # aktualizacja wag zgodnie ze wzorem z powyższego obrazka.
-  print("Learning")
-  for i in tqdm.tqdm(range(n_iterations)):
-      aproximation_function = get_approximation(w, c, r)
-      for sample, label in zip(X, y):
-          y_pred = aproximation_function(sample)
-          for j in range(len(w)):
-              w[j] = w[j] + l_r * (label - y_pred) * phi(np.linalg.norm(sample - c[j]))
-
-  return w, c, r
-
-
+# WCZYTYWANIE DANYCH
 path = "KIM_data.csv"
 data = pd.read_csv(path)
 
-# Dane wejściowe X
 X = data[["high", "low", "close", "volume"]].values
-# Dane wyjściowe y (Będziemy opisywać wszystkie ceny otwarcia).
 y = data[["open"]].values
 
 X_cpy = X.copy()
@@ -182,66 +161,39 @@ for i in range(len(X)):
     else:
         X[i] = "NaN"
         y[i] = "NaN"
-
+# usunięcie niepełnych danych
 X = X[5:]
-#X = X.reset_index(drop=True)
-
 y = y[5:]
-#y = y.reset_index(drop=True)
-# Podział danych na dane testowe oraz dane treningowe.
+
+# Tworzenie zbioru testowego i treningowego
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
-# normalizacja danych.
+# normalizacja
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
+# ------------------------------ wywołanie -------------------------------#
 
-# wywołanie algorytmu algebraicznego.
-w, c, r = RBF_approximation(X_train, y_train, p=20)
-print('WSZMATEN\n', w)
-# stworzenie funkcji aproksymującej. Funkcja na podstawie dobranych wag przewidzi wszystkie ceny otwarcia.
-aproximation_function = get_approximation(w, c, r)
+# trening
+w, c, r = RBF_iter(X_train, y_train, p=20)
+approx_fcn = get_approx(w, c, r)
 
-# predykcja dla danych wejściowych X_train. Predykcja przypiszę cenę otwarcia (klasteryzacja jest działaniem pośrednim w tym wszystkim).
-# Predykcja jest to jedynie powielenie wyjść funkcji approximation_function.
-predictions = [aproximation_function(x) for x in X_train]
-# Błąd średniokwadratowy (mse).
-error = np.sum([(prediction - label) ** 2 for prediction, label in zip(predictions, y_train)]) / len(predictions)
+# # predykcje dla zbioru treningowego
+# predictions = [approx_fcn(x) for x in X_train]
+# error = np.sum([(prediction - label) ** 2 for prediction, label in zip(predictions, y_train)]) / len(predictions)
+#
+# print("MSE - training data = ", error)
+# plt.scatter(y_train, predictions)
+# plt.show()
 
-print("Bład mse dla danych treningowych = ", error)
-
-# Wykres ilustrujący poprawność zadziałania algorytmu
-# oś x - rzeczywista odpowiedź
-# oś y - predykcja
-# Gdyby algorytm działał idealnie, wykres byłby idealnie na przekątnej.
-plt.scatter(y_train, predictions)
-plt.show()
-
-predictions = [aproximation_function(x) for x in X_test]
+# predykcje dla zbioru testowego (czyli ta istotna część)
+predictions = [approx_fcn(x) for x in X_test]
 error = np.sum([(prediction - label) ** 2 for prediction, label in zip(predictions, y_test)]) / len(predictions)
-
-print("Bład mse dla danych testowych = ", error)
-plt.scatter(y_test, predictions)
+error_abs = [(abs(prediction - label)*100)/label for prediction, label in zip(predictions, y_test)]
+plt.plot(error_abs)
 plt.show()
 
-w, c, r = RBF_approximation_iter(X_train, y_train, p=20)
-print('WWWWWWWWWWWWW', w)
-
-aproximation_function = get_approximation(w, c, r)
-
-predictions = [aproximation_function(x) for x in X_train]
-error = np.sum([(prediction - label) ** 2 for prediction, label in zip(predictions, y_train)]) / len(predictions)
-
-print("Bład mse dla danych treningowych = ", error)
-
-plt.scatter(y_train, predictions)
-plt.show()
-
-predictions = [aproximation_function(x) for x in X_test]
-error = np.sum([(prediction - label) ** 2 for prediction, label in zip(predictions, y_test)]) / len(predictions)
-
-print("Bład mse dla danych testowych = ", error)
-
+print("MSE - test data = ", error)
 plt.scatter(y_test, predictions)
 plt.show()
